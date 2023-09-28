62cbf8484fef5fbcf854cbb46716fdecdeb3c66f
ValdecarlosLZ <101909600+ValdecarlosLZ@users.noreply.github.com>
2023-09-28 09:11:53 -0400

    .

---
+++ switch not null.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { InputSwitch } from "primereact/inputswitch";
+
+export default function PreselectionDemo() {
+    const [checked, setChecked] = useState(true);
+
+    return (
+        <div className="card flex justify-content-center">
+            <InputSwitch checked={checked} onChange={(e) => setChecked(e.value)} />
+        </div>
+    );
+}
+        
